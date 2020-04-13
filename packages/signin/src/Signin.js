ab9db66e7f5b9f99f54954b2be715d87dffdbaf1
Min Kim <minkimcello@gmail.com>
2020-04-13 14:24:38 -0400

    Convert repo to monorepo and add Signin app (#1)
    * Add signin app and convert repo to monorepo
    
    Add workspace specs in package.json
    Change monorepo package name to @bigtest/example-monorepo
    Move todomvc test into packages as @bigtest/example-todomvc
    Migrate signin app to packages as @bigtest/example-signin
    Add signin-test.ts and tsconfig.json to signin app
    
    * Update monorepo package name and remove start script

---
+++ packages/signin/src/Signin.js
@@ -0,0 +1,78 @@
+import React, { useState } from 'react';
+import './Signin.css';
+
+const App = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [credentials, setCredentials] = useState({});
+
+  const onSubmit = (username, password) => {
+    setIsAuthenticated(true);
+    setIsModalOpen(false);
+    setCredentials({ username, password });
+  };
+
+  return (
+    <div className="App">
+      {isAuthenticated &&
+        <p data-testid="welcome-text">
+          Hello {credentials.username}.
+        </p>
+      }
+      <button
+        className="sign-in-button"
+        // onClick={() => setIsModalOpen(true)} // disable this
+        style={{ display: !isAuthenticated ? "block" : "none" }}
+        data-testid="sign-in-button"
+      >
+        SIGN IN
+      </button>
+      {isModalOpen && <Modal
+        onSubmit={onSubmit}
+      />}
+    </div>
+  );
+}
+
+const Modal = ({
+  onSubmit
+}) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  return (
+    <form
+      onSubmit={e => {
+        e.preventDefault();
+        onSubmit(username, password)
+      }}
+      className="modal"
+      data-testid="modal"
+    >
+      <input
+        className="modal-input"
+        placeholder="USERNAME"
+        type="text"
+        onChange={e => setUsername(e.target.value)}
+        value={username}
+        data-testid="modal-username"
+      />
+      <input
+        className="modal-input"
+        placeholder="PASSWORD"
+        type="password"
+        onChange={e => setPassword(e.target.value)}
+        value={password}
+      />
+      <button
+        className="modal-submit"
+        type="submit"
+        data-testid="modal-submit"
+      >
+        SUBMIT
+      </button>
+    </form>
+  )
+};
+
+export default App;
