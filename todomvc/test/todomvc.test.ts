be7a2f798ebd7c92eb4b17c516032fed53b5027c
Jonas Nicklas <jonas@jnicklas.com>
2020-05-13 20:08:56 +0200

    Actually make the test work
    Even though it's hacky AF

--- todomvc/test/todomvc.test.ts
+++ todomvc/test/todomvc.test.ts
@@ -1,13 +1,21 @@
+import 'regenerator-runtime/runtime';
 import { test } from '@bigtest/suite';
 import { strict as assert } from 'assert';
 
 export default test("TodoMVC")
   .child("When page is initially opened", test => test
+    .step("it focuses on the todo input field", async () => {
+      let input;
+      while(!(input = document.querySelector<HTMLInputElement>("[placeholder='What needs to be done?']"))) {
+        console.log("INPUT", input);
+        await new Promise((resolve) => setTimeout(resolve, 10));
+      }
+    })
     .assertion("it focuses on the todo input field", () => {
-      // let input = document.querySelector<HTMLInputElement>("[placeholder='What needs to be done?]");
+      let input = document.querySelector<HTMLInputElement>("[placeholder='What needs to be done?']");
 
-      // assert(input, 'Cannot find TODO input element');
-      // assert.equal(document.activeElement, input,
-      //   `Expected ${input} to have focus, but it did not. Instead, it was ${document.activeElement}`);
+      assert(input, 'Cannot find TODO input element');
+      assert.equal(document.activeElement, input,
+        `Expected ${input} to have focus, but it did not. Instead, it was ${document.activeElement}`);
     })
   );
