f3a8d02449358f9d4953992473e77341cbc0e293
Charles Lowell <cowboyd@frontside.io>
2020-05-16 07:55:54 -0500

    Use the threaded context and step timeouts from unreleased bigtest

--- todomvc/test/todomvc.test.ts
+++ todomvc/test/todomvc.test.ts
@@ -3,19 +3,17 @@ import { test } from '@bigtest/suite';
 import { strict as assert } from 'assert';
 
 export default test("TodoMVC")
-  .child("When page is initially opened", test => test
-    .step("it focuses on the todo input field", async () => {
-      let input;
-      while(!(input = document.querySelector<HTMLInputElement>("[placeholder='What needs to be done?']"))) {
-        console.log("INPUT", input);
-        await new Promise((resolve) => setTimeout(resolve, 10));
-      }
-    })
-    .assertion("it focuses on the todo input field", () => {
-      let input = document.querySelector<HTMLInputElement>("[placeholder='What needs to be done?']");
-
-      assert(input, 'Cannot find TODO input element');
-      assert.equal(document.activeElement, input,
-        `Expected ${input} to have focus, but it did not. Instead, it was ${document.activeElement}`);
-    })
-  );
+  .child(
+    "When page is initially opened", test => test
+      .step("find input field", async () => {
+        let input;
+        while(!(input = document.querySelector<HTMLInputElement>("[placeholder='What needs to be done?']"))) {
+          await new Promise((resolve) => setTimeout(resolve, 10));
+        }
+        return { input };
+      })
+      .assertion("it focuses on the todo input field", ({ input }) => {
+        assert.equal(
+          document.activeElement, input,
+          `Expected ${input} to have focus, but it did not. Instead, it was ${document.activeElement}`);
+      }));
