d910431273900b07f9dd6683195d3f862e8d901c
Jonas Nicklas <jonas@jnicklas.com>
2020-07-02 15:13:03 +0200

    Actually start using interactors for todomvc

--- todomvc/test/todomvc.test.ts
+++ todomvc/test/todomvc.test.ts
@@ -1,21 +1,41 @@
 import 'regenerator-runtime/runtime';
 import { test } from '@bigtest/suite';
 import { strict as assert } from 'assert';
+import { App, createInteractor } from '@bigtest/interactor';
+
+const TextField = createInteractor<HTMLInputElement>('text field')({
+  selector: 'input[type=text]',
+  locators: {
+    byPlaceholder: (element) => element.placeholder
+  },
+  actions: {
+    fillIn: (element, value: string) => element.value = value,
+    pressEnter: (element) => {
+      let init: any = { bubbles: true, key: 'Enter', code: 'Enter', which: 13, keyCode: 13 };
+      element.dispatchEvent(new KeyboardEvent('keydown', init));
+      element.dispatchEvent(new KeyboardEvent('keyup', init));
+    }
+  }
+});
+
+const ListItem = createInteractor<HTMLLIElement>('list item')({
+  selector: 'li',
+});
+
+const TodoField = TextField.byPlaceholder('What needs to be done?');
 
 export default test("TodoMVC")
   .child("When page is initially opened", test => test
-    .step("it focuses on the todo input field", async () => {
-      let input;
-      while(!(input = document.querySelector<HTMLInputElement>("[placeholder='What needs to be done?']"))) {
-        console.log("INPUT", input);
-        await new Promise((resolve) => setTimeout(resolve, 10));
-      }
+    .step("loading the app", async () => {
+      await App.visit();
     })
-    .assertion("it focuses on the todo input field", () => {
-      let input = document.querySelector<HTMLInputElement>("[placeholder='What needs to be done?']");
-
-      assert(input, 'Cannot find TODO input element');
-      assert.equal(document.activeElement, input,
-        `Expected ${input} to have focus, but it did not. Instead, it was ${document.activeElement}`);
+    .step("filling in the todo field", async () => {
+      await TodoField.fillIn('Write some tests');
+    })
+    .step("and pressing enter", async () => {
+      await TodoField.pressEnter();
+    })
+    .assertion("there should be a new item", async () => {
+      await ListItem('Write some tests').exists();
     })
   );
